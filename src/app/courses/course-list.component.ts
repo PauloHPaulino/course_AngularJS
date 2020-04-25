b295cf7e045af474a0d67f27adcb6da8a24e18ca
PauloHPaulino <63624621+PauloHPaulino@users.noreply.github.com>
2020-04-25 17:59:34 -0300

    Add New Files

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+
+@Component({
+    selector: 'app-course-list ',
+    templateUrl: './course-list.component.html'
+})
+
+export class CourseListComponent {
+    
+
+    courses: Course[]=[];
+  
+   
+
+}
+
